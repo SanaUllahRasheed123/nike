7ba6124461e64f9aba5c61ca06375bdcd2ac0428
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-11-02 01:08:33 +0500

    Hero section is completed

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <main className="relative">
     {/* <nav/> */}
     <Nav/>
-    <section className="xl:paddign-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero/>
     </section>
     <section className="padding"><PopularProducts/></section>
